6538498bdfc3275251b0474414487a2d7676b808
Tyler Ewald <tewald@olin.edu>
2022-04-26 10:33:15 -0400

    update servo class to be named motor class

--- Motor.m
+++ Motor.m
@@ -1,11 +1,11 @@
-classdef Servo
+classdef Motor
    %class to control the servos on board the robot (camera pan and steering)
    properties
      robotServo;
    end
    
    methods
-       function obj = Servo(robotArduino, servoPin)
+       function obj = Motor(robotArduino, servoPin)
            %initalizes the function and sets up the variables to use
            obj.robotServo = servo(robotArduino, servoPin, "MinPulseDuration", 1466 * 10 ^ -6, "MaxPulseDuration", 1536 * 10 ^ -6);
        end
@@ -15,6 +15,11 @@ classdef Servo
             writePosition(obj.robotServo, moveAngle);
        end
        
+       function moveMotor(obj, moveAngle)
+            %moves the motor (used so not to confused the 2)
+            writePosition(obj.robotServo, moveAngle)
+       end
+
        function currentPosition = getPosition(obj)
            %returns the angle that the servo is at
             currentPosition = readPosition(obj.robotServo);
